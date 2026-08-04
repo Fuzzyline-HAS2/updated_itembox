41f08eedba3659be7f2adceb27017fa2dd8d7c4e
bottlejun <jysbruh0602@gmail.com>
2026-08-04 22:54:44 +0900

    Firmware Update v32 + Partition v8

--- updated_itembox.ino
+++ updated_itembox.ino
@@ -9,8 +9,8 @@
  *
  */
 
-#define FIRMWARE_VER 31
-#define PARTITION_VER 7
+#define FIRMWARE_VER 32
+#define PARTITION_VER 8
 #include "updated_itembox.h"
 #include "esp_system.h"
 
@@ -18,7 +18,8 @@ void setup()
 {
     Serial.begin(115200);
 
-    has2wifi.Setup("badland");
+    // has2wifi.Setup("badland");
+    has2wifi.Setup("city");
     // has2wifi.Setup("badland_ruins","Code3824@");
     has2wifi.Send((String)(const char*)my["device_name"], "esp_version", String(FIRMWARE_VER)); // 부팅 시 현재 펌웨어 버전을 서버 device.esp_version에 보고 (OTA 재부팅 후에도 자동 갱신)
     TelnetInit();
